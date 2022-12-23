cf8f508730df3ffff444d1f50b453fc13b4741f4
hbujakow <hbujakow@gmail.com>
2022-12-23 19:08:08 +0100

    add ex test

--- core/movierecommenderapp/tests.py
+++ core/movierecommenderapp/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class ViewTest(TestCase):
+    def test_view(self):
+        response = self.client.get(reverse('hello'))
+        self.assertEqual(response.status_code, 200)
--- core/movierecommenderapp/urls.py
+++ core/movierecommenderapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello/', views.say_hello),
+    path('hello/', views.say_hello, name='hello'),
     path('search/', views.search),
     path('omdb/', views.omdb_search)
-]
+]   

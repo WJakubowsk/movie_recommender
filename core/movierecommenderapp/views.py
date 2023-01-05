fc9f8717c3d8c46b495b92b631df79c9cb5bc717
hbujakow <hbujakow@gmail.com>
2023-01-05 15:47:20 +0100

    add views

--- core/movierecommenderapp/views.py
+++ core/movierecommenderapp/views.py
@@ -1,21 +1,24 @@
 from django.shortcuts import render
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 from omdb import OMDBClient
+from .models import Show
 
-omdb_api = OMDBClient(apikey="730a97c3")
-
+omdb_api = OMDBClient(apikey="730a97c3") # can stay for now
 
 def example(request, title):
     movies = omdb_api.get(search=title)
-    return render(request, "omdb_search.html", {"movies": movies, "title": title})
+    return render(request, "example.html", {"movies": movies, "title": title})
 
 
 def home(request):
-    return render(request, 'home.html')
+    movies = Show.objects.all()
+    return render(request, 'home.html', {'movies': movies})
 
+def signup(request):
 
-def signup():
-    return None
-
+    form = UserCreationForm()
+    return render(request, 'signUp.html', {'form' : form})
 
 def login():
     return None

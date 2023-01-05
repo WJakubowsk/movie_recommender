62dd67b5cbf2ae932914cc4b1c423088d63a0230
hbujakow <hbujakow@gmail.com>
2023-01-05 01:41:07 +0100

    add simple database & models

--- core/movierecommenderapp/urls.py
+++ core/movierecommenderapp/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('hello/', views.say_hello, name='hello'),
-    path('search/', views.search),
-    path('omdb/<str:title>/', views.omdb_search)
+    path('omdb/<str:title>/', views.omdb_search),
+    path('', views.home, name='home'),
 ]
--- core/movierecommenderapp/views.py
+++ core/movierecommenderapp/views.py
@@ -1,20 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from omdb import OMDBClient
 
-moviesDB = OMDBClient(apikey="730a97c3")
+omdb_api = OMDBClient(apikey="730a97c3")
 
 
 def say_hello(request):
     return render(request, 'hello.html')
 
-
-def search(request):
-    movies = ['Matrix', 'The Matrix',
-              'The Matrix Reloaded', 'The Matrix Revolutions']
-    return render(request, 'search.html', {'movies': movies})
-
-
 def omdb_search(request, title):
-    movies = moviesDB.get(search=title)
-    return render(request, "omdb_search.html", {"movies": movies ,"title": title})
+    movies = omdb_api.get(search=title)
+    return render(request, "omdb_search.html", {"movies": movies, "title": title})
+
+
+def home(request):
+    return render(request, 'home.html')

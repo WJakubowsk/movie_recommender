c85280d65083f41bbd6749af3eee379d4b0a7fe7
hbujakow <hbujakow@gmail.com>
2023-01-07 00:27:42 +0100

    add full registration, login, logout interface, session handling

--- core/movierecommenderapp/urls.py
+++ core/movierecommenderapp/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('home/', views.index, name='index'),
     path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
     path('logout/', views.logout, name='logout'),
     path('search/', views.search, name='search'),
     path('recommend/', views.recommend, name='recommend'),
--- core/movierecommenderapp/views.py
+++ core/movierecommenderapp/views.py
@@ -1,31 +1,59 @@
-from django.shortcuts import render
-from django.contrib.auth import login, authenticate
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth import login, authenticate, get_user_model
 from django.contrib.auth.forms import UserCreationForm
 from omdb import OMDBClient
 from .models import Show
 
-omdb_api = OMDBClient(apikey="730a97c3") # can stay for now
+omdb_api = OMDBClient(apikey="730a97c3")  # can stay for now
+
 
 def example(request, title):
     movies = omdb_api.get(search=title)
     return render(request, "example.html", {"movies": movies, "title": title})
 
 
-def home(request):
+def index(request):
+    return render(request, 'index.html')
+
+
+def home(request):  # TODO swap with index
     movies = Show.objects.all()
     return render(request, 'home.html', {'movies': movies})
 
+
 def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        firstname = request.POST['firstname']
+        lastname = request.POST['lastname']
+        user = get_user_model().objects.create_user(username=username, email=email, password=password,
+                                                    first_name=firstname, last_name=lastname)
+        login(request, user)
+        return HttpResponse('Signed up')  # TODO redirect to home
+    else:
+        return render(request, 'signUp.html')
 
-    form = UserCreationForm()
-    return render(request, 'signUp.html', {'form' : form})
 
-def login():
-    return None
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            # return HttpResponse('Logged in') # TODO redirect to home
+            return redirect('index')
+        else:
+            return HttpResponse('Invalid username or password')
+    else:
+        return render(request, 'login.html')
 
 
-def logout():
-    return None
+def logout(request):
+    logout(request)
+    return redirect('home')
 
 
 def search():

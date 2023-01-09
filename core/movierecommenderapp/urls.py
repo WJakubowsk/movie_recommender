7d698231a008bf954dce78b53d908973874ec6c1
hbujakow <hbujakow@gmail.com>
2023-01-09 15:41:00 +0100

    fix urls, remove linkedin stats, add repo link

--- core/movierecommenderapp/urls.py
+++ core/movierecommenderapp/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path('recommend/', views.recommend, name='recommend'),
     path('info/', views.info, name='info'),
     path('omdb/<str:title>/', views.example, name='example'),
-    path('about', views.about, name='about'),
+    path('about/', views.about, name='about'),
 ]
